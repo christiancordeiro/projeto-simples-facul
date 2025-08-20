d4bb316907054c6512abb2c56d70123b7f5e0db9
christiancordeiro <christiancordeiro96@gmail.com>
2025-08-20 00:33:55 -0300

    modified by vercel

--- script.js
+++ script.js
@@ -4,25 +4,30 @@ const movies = document.createElement('ul');
 movies.classList.add('moviesDiv');
 main.appendChild(movies);
 
-fetch(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR`
-)
+fetch('/api/key')
     .then((response) => response.json())
     .then((data) => {
-        data.results.forEach((movie) => {
-            const movieElement = document.createElement('li');
-            movieElement.classList.add('movie');
-            const baseUrl = 'https://image.tmdb.org/t/p/w500';
-            const posterPath = movie.poster_path;
-            const imgSrc = `${baseUrl}${posterPath}`;
-            movieElement.classList.add('movie');
-            movieElement.innerHTML = `
+        const API_KEY = data.apiKey;
+        fetch(
+            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=pt-BR`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                data.results.forEach((movie) => {
+                    const movieElement = document.createElement('li');
+                    movieElement.classList.add('movie');
+                    const baseUrl = 'https://image.tmdb.org/t/p/w500';
+                    const posterPath = movie.poster_path;
+                    const imgSrc = `${baseUrl}${posterPath}`;
+                    movieElement.classList.add('movie');
+                    movieElement.innerHTML = `
                 <img src="${imgSrc}" alt="${movie.title}" />
             `;
 
-            movieElement.addEventListener('click', () => {
-                window.location.href = `movie.html?id=${movie.id}`;
+                    movieElement.addEventListener('click', () => {
+                        window.location.href = `movie.html?id=${movie.id}`;
+                    });
+                    movies.appendChild(movieElement);
+                });
             });
-            movies.appendChild(movieElement);
-        });
     });
